0e6a78c1f0ab7a49323015f4c84207916ef3df96
SanviBhelkar <115478811+SanviBhelkar@users.noreply.github.com>
2024-04-10 22:20:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './App'; // Assuming your root component is App.js
+
+// Register the root component
+AppRegistry.registerComponent('MyApp', () => App);
